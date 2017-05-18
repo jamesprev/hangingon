5342390a4bdfd7abf81a6ba82480f2d694814146
abel445 <belekas93@gmail.com>
2017-05-19 00:23:42 +1000

    Added basic POJOs (Customer, Flight, FlightSeat)

---
+++ assign/src/java/flightclub/Customer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightclub;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Customer {
+    public String firstName;
+    public String lastName;
+    public String userName;
+}
---
+++ assign/src/java/flightclub/FlightSeat.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightclub;
+
+/**
+ *
+ * @author Andrew
+ */
+public class FlightSeat {
+
+    private int row;
+    private String seat;
+
+    public int getRow() {
+        return row;
+    }
+
+    public String getSeat() {
+        return seat;
+
+    }
+
+}
