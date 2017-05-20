73a2d225ec6df391a451183994cf36f7ad669787
11987839 <11987839@e1-b1400-20.ad.feit.uts.edu.au>
2017-05-20 15:10:17 +1000

    User.java xml'd & @param'd. Register has admin selection Menus added to pages

--- assign/src/java/flightclub/User.java
+++ assign/src/java/flightclub/User.java
@@ -1,35 +1,52 @@
 package flightclub;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  * 
  * @author Harrison
  */
-public class User {
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class User implements Serializable{
 
    //Need to convert this to a bean
     
     //Add all the stuff a user is meant to have including username/password
     
-    private boolean isAdmin;
+    private boolean isadmin;
+    @XmlElement(name = "name")
     private String name;
+    @XmlElement(name = "email")
     private String email;
+    @XmlElement(name = "password")
     private String password;
+    @XmlElement(name = "dob")
     private String dob;
-    
+    @XmlElement(name = "usertype")
+    private String usertype;
     
     /**
      * Empty constructor to be a javabean
+     * @param name
+     * @param email
+     * @param password
+     * @param dob
+     * @param usertype
      */
-    public User() {}
+    public User(String name, String email, String password, String dob, String usertype) {}
     
     public boolean getIsAdmin() {
-        return isAdmin;
+        return isadmin;
     }
     
     public String getName() {
@@ -48,11 +65,16 @@ public class User {
         return dob;
     }
     
+    public String getUsertype(){
+        return usertype;
+    }
+    
+    
     /**
      * @param isAdmin the isAdmin to set
      */
     public void setIsAdmin(boolean isAdmin) {
-        this.isAdmin = isAdmin;
+        this.isadmin = isAdmin;
     }
 
     /**
@@ -81,5 +103,12 @@ public class User {
      */
     public void setDob(String dob) {
         this.dob = dob;
+    }
+    
+    /**
+     * @param usertype the usertype to set
+     */
+        public void setUsertype(String usertype) {
+        this.usertype = usertype;
     }
 }
